ba751f94d50ff584be77ddbb250993ef60e1a1af
ireneboichuk <sun.shine89560@gmail.com>
2021-04-04 22:54:50 +0300

    HomeTask 8.2

--- HT_8.2/Program.cs
+++ HT_8.2/Program.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using System.Linq;
 
 /* 8.1
 1) Create abstract class Shape with field name and property Name. 
@@ -21,6 +22,8 @@ namespace Test
             List<double> shapes = new List<double>();
             List<Circle> circles = new List<Circle>();
             List<Square> squares = new List<Square>();
+            Dictionary<double, string> perimeters = new Dictionary<double, string>(10);
+            Dictionary<double, string> areas = new Dictionary<double, string>(10);
 
             Console.WriteLine("Введіть 10 значень, які будуть використані для радіуса та сторони квадрата: ");
             for (int n = 0; n < 11; n++)
@@ -34,12 +37,13 @@ namespace Test
                 squares.Add(new Square() { Side = shapes[n] });
             }
 
-
             foreach (Circle p in circles)
             {
                 Console.WriteLine($"Для кола з радіусом: {p.Radius}, значення площі дорівнює: {p.Area(p.Radius)} " +
                     $"значення периметру дорівнює: {p.Perimeter(p.Radius)}"
                     );
+                perimeters.Add(p.Perimeter(p.Radius), "Коло");
+                areas.Add(p.Area(p.Radius), "Коло");
             }
 
             Console.WriteLine("-------------");
@@ -49,6 +53,20 @@ namespace Test
                 Console.WriteLine($"Для квадрата з стороною: {p.Side}, значення площі дорівнює: {p.Area(p.Side)} " +
                     $"значення периметру дорівнює: {p.Perimeter(p.Side)} "
                     );
+                perimeters.Add(p.Perimeter(p.Side), "Квадрат");
+                areas.Add(p.Area(p.Side), "Коло");
+            }
+
+            //Пошук і виведення максимального значення
+            var maxPair = (from d in perimeters select d.Key).Max();
+            Console.WriteLine($"Найбільше значення периметру має:  {maxPair}, назва фігури: {perimeters[Convert.ToDouble(maxPair)]}");
+
+            //Сортування за площею та виведення списку
+            Console.WriteLine("Відсортований список площ: ");
+            var sortedAreas = new SortedDictionary<double, string>(areas);
+            foreach (var value in sortedAreas)
+            {
+                Console.WriteLine("Key: " + value.Key + "; Value: " + value.Value);
             }
 
             Console.ReadLine();
